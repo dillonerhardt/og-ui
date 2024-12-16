9c9828762d001bc326786177b1a56989fe459998
Dillon Erhardt <derhardt@contractor.indeed.com>
2024-12-16 13:29:20 +0000

    Add event example bg

--- app/template/components/event-og.tsx
+++ app/template/components/event-og.tsx
@@ -33,15 +33,17 @@ export default function EventOg({
         <img
           src={backgroundImage}
           alt="Background"
+          //   height={1000}
+          //   width={1000}
           style={{
             position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-            objectFit: "cover",
+            // top: 0,
+            // left: 0,
+            // width: "100%",
+            // height: "100%",
+            objectFit: "contain",
             objectPosition: "center",
-            zIndex: 0,
+            // zIndex: 0,
           }}
         />
       )}
